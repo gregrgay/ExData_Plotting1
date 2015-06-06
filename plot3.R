f38ea1e19efd9e11f79eed1f732d3d6b400bde70
Greg Gay <gregrgay@gmail.com>
2015-06-05 20:01:25 -0400

    adjust size and padding to make the legend fit better

--- plot3.R
+++ plot3.R
@@ -36,7 +36,14 @@ plot(as.numeric(c2007$Sub_metering_3),
 )
 lines(c2007$Sub_metering_2, lty=1, type="l", col="red" )
 lines(c2007$Sub_metering_1, lty=1, type="l", col="black" )
-legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="n", lty=c(1,1), col=c("black","red","blue"))
+legend("topright", 
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
+       bty="n", 
+       lty=c(1,1), 
+       col=c("black","red","blue"),
+       cex=.6,  
+       pt.cex = 1,
+       inset=0.05)
 axis(1,at=c(0,1500,2900),labels=c("thur","fri","sat"))
-#dev.copy(png, file = "plot3.png")  ## Copy the plot to a PNG file
-#dev.off()
+dev.copy(png, file = "plot3.png")  ## Copy the plot to a PNG file
+dev.off()
